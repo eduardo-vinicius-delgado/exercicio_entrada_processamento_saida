6c9b9b67185cca0da70c453bcae0d125231e3c5c
Eduardo Vinícius Delgado <eduardo.vini.delgado@gmail.com>
2022-08-04 21:56:55 -0300

    atualizações

--- application/Main.java
+++ application/Main.java
@@ -7,24 +7,15 @@ public class Main {
     public static void main(String[] args){
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
-        int valor1 = 0, valor2 = 0, somaValores = 0;
-        double multiplicacaoValores = 0.0, divisaoValores = 0.0, exponenciacaoValores = 0.0, radiciacaoValores = 0.0;
-        System.out.print("Digite o primeiro valor: ");
-        valor1 = sc.nextInt();
-        System.out.print("Digite o segundo valor: ");
-        valor2 = sc.nextInt();
-        somaValores = valor1 + valor2;
-        System.out.println("Soma dos valores: "+somaValores);
-        multiplicacaoValores = valor1 * valor2;
-        System.out.printf("Multiplicacao dos valores: %.2f\n",multiplicacaoValores);
-        divisaoValores = (double) valor1/valor2;
-        System.out.printf("Divisao do valor 1 pelo valor 2: %.2f\n",divisaoValores);
-        exponenciacaoValores = Math.pow(valor1, valor2);
-        System.out.printf("Valor 1 elevado ao valor 2: %.2f \n", exponenciacaoValores);
-        radiciacaoValores = Math.pow(valor1,1/(double) valor2);
-        System.out.printf("Valor 1 como raiz quadrada do valor 2: %.2f", radiciacaoValores);
-
+        showMensage("Digite o primeiro valor: ");
+        int valor1 = sc.nextInt();
+        showMensage("Digite o primeiro valor: ");
         sc.close();
     }
+
+    public static void showMensage(String a){
+        System.out.printf(a);
+    }
+
 }
     
